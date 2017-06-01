7e51598763901eb6d5b7156233b6b280f65318ef
Mar, G <gfmarster@gmail.com>
2017-06-01 02:03:01 +0000

    desenved

--- gulpfile.js
+++ gulpfile.js
@@ -4,40 +4,49 @@
 
     // DEPENDENCIES
 
-    const gulp   = require('gulp'),
+    const
+        gulp     = require('gulp'),
         clean    = require('gulp-clean'),
+        concat   = require('gulp-concat'),
         eslint   = require('gulp-eslint'),
         minify   = require('gulp-minify'),
         sequence = require('run-sequence');
+
+    const
+        pack = require('./package.json');
 
     // AUXILIARY
 
     const paths = {
         source : ['gulpfile.js', './source/**/*.js'],
-        build  : ['./source/**/*.js'],
-        dist   : 'dist'
+        build  : [
+            './source/utils.js',
+            './source/modules/utils.js',
+            './source/modules/params-schema.js',
+            './source/modules/utils.js'
+        ],
+        dist : 'dist'
     };
 
     // TASKS
 
-    gulp.task('clean', function taskClean (cb) {
-        gulp.src(paths.dist).pipe(clean()).on('end', cb);
+    gulp.task('clean', function taskClean () {
+        return gulp.src(paths.dist).pipe(clean());
     });
 
-    gulp.task('validate', function validate (cb) {
-        gulp.src(paths.source)
+    gulp.task('validate', function validate () {
+        return gulp.src(paths.source)
             .pipe(eslint())
             .pipe(eslint.format())
-            .pipe(eslint.failAfterError())
-            .on('end', cb);
+            .pipe(eslint.failAfterError());
     });
 
-    gulp.task('build', function build (cb) {
-        sequence(['clean', 'validate'], function sequenced () {
+    gulp.task('build', function build () {
+        return sequence('clean', 'validate', () => {
             return gulp.src(paths.build)
+                .pipe(concat(`sea-${pack.version}.js`))
                 .pipe(minify( {ext: {min: '.min.js'}} ))
-                .pipe(gulp.dest(paths.dist))
-                .on('end', cb);
+                .pipe(gulp.dest(paths.dist));
         });
     });
 
